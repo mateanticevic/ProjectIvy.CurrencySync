8ff2a2615ff0ecd64d54c6e03965df9c2d48417a
Mate Antičević <mate@anticevic.net>
2021-10-10 17:57:54 +0200

    Serilog added

--- src/ProjectIvy.CurrencySync/Program.cs
+++ src/ProjectIvy.CurrencySync/Program.cs
@@ -1,6 +1,10 @@
 ﻿using ProjectIvy.CurrencySync.Services.Fixer;
 using System.Threading.Tasks;
 using System;
+using Serilog;
+using Serilog.Sinks.Graylog;
+using Serilog.Events;
+using Serilog.Sinks.Graylog.Core.Transport;
 
 namespace ProjectIvy.CurrencySync
 {
@@ -8,25 +12,41 @@ namespace ProjectIvy.CurrencySync
     {
         public async static Task Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+                                      .MinimumLevel.Override(nameof(Microsoft), LogEventLevel.Information)
+                                      .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                                      .Enrich.FromLogContext()
+                                      .WriteTo.Console()
+                                      .WriteTo.Graylog(new GraylogSinkOptions()
+                                      {
+                                          Facility = "project-ivy-currency-sync",
+                                          HostnameOrAddress = "10.0.1.24",
+                                          Port = 12202,
+                                          TransportType = TransportType.Udp
+                                      })
+                                      .CreateLogger();
+
+            Log.Logger.Information("Application started");
+
             string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 
-            var date = DateTime.Now.Date;
-            var last = await DbHandler.GetLastExchangeRateDate(connectionString);
+            var date = await DbHandler.GetLastExchangeRateDate(connectionString);
 
-            while (date > last)
+            while (date < DateTime.Now.Date)
             {
                 try
                 {
                     await ResolveRatesOnDay(connectionString, date);
 
-                    Console.WriteLine($"Day {date:yyyy-MM-dd} ok.");
+                    Log.Logger.Information("Currencies synced for {Date}", date.ToString("yyyy-MM-dd"));
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine($"Something went wrong, message: {e.Message}");
+                    Log.Logger.Error(e, "Currency sync failed for {Date}", date.ToString("yyyy-MM-dd"));
                 }
 
-                date = date.AddDays(-1);
+                date = date.AddDays(1);
             }
         }
 
